4f9c11c2d28754bd37f8992a54ab1908a9d0926b
paulhth <paul.horvath@student.upt.ro>
2024-05-23 22:13:58 +0300

    Review done, remarks in new 4 and further work

--- src/config/config.h
+++ src/config/config.h
@@ -1,7 +1,7 @@
-#define WIFI_SSID "THE BRACELET"
-#define WIFI_PASS "2010#PepsiHTH"
-// #define WIFI_SSID "Aghiz Love"
-// #define WIFI_PASS "bucatar56"
+//#define WIFI_SSID "THE BRACELET"
+//#define WIFI_PASS "2010#PepsiHTH"
+ #define WIFI_SSID "Aghiz Love"
+ #define WIFI_PASS "bucatar56"
 //#define WIFI_SSID "iPhone Station"
 //#define WIFI_PASS "blocuri13"
 //#define WIFI_SSID "Oratoriu_2.4"
@@ -18,7 +18,7 @@
  *
  * @warning The value of this macro will determine the behavior of the web server. It can either display the real-time sensor data or the testing data.
  */
-#define SERVER_TESTING false
+#define SERVER_TESTING true
 
 #define ELM_PORT SerialBT
 #define DEBUG_PORT Serial
